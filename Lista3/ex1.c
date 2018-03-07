70100943f5797bba9c33d8183578afb71f619809
FilipiBrabo <filipi.brabo@gmail.com>
2018-03-06 22:04:15 -0300

    ex1.4 finalizado

--- Lista3/ex1.c
+++ Lista3/ex1.c
@@ -32,24 +32,23 @@ void printList(pnode *head){
 }
 
 void insertPosX(pnode * head, pnode x, int pos){
-	pnode aux = *head;
 	if (pos == 1){		
-		x->next = *head;
-		*head = x;
-		free(aux);
-	}
-	for (int i = 2; i < pos; i++){
-		if (aux == NULL)
-			printf("Não existe essa posição\n");
-		else{
-			aux = aux->next;
+		insertNode(head, x);
+	}else{
+		pnode aux = *head;
+		for (int i = 2; i < pos; i++){
+			if (aux == NULL)
+				printf("Não existe essa posição\n");
+			else{
+				aux = aux->next;
+			}
 		}
-	}
-	if (aux == NULL){
-		printf("Escolha um posição válida\n");
-	} else{
-		x->next = aux->next;
-		aux->next = x;
+		if (aux == NULL){
+			printf("Escolha um posição válida\n");
+		} else{
+			x->next = aux->next;
+			aux->next = x;
+		}
 	}
 }
 
@@ -59,7 +58,7 @@ void removePosX(pnode * head, int pos){
 	} else if(pos == 1){
 		pnode aux = *head;
 		*head = (*head)->next;
-		free(aux);		
+		//free(aux);		
 	} else{
 		pnode aux = *head;
 		for (int i = 2; i < pos; i++){
@@ -78,7 +77,7 @@ void removePosX(pnode * head, int pos){
 				aux->next = NULL;
 		} 	else{
 				aux->next = x->next;
-				free(x);
+				//free(x);
 			}
 	 	}
 	}
@@ -97,18 +96,46 @@ void invertList(pnode *head){
 	*head = prev;	
 }
 
+pnode splitList(pnode *head){
+	pnode current = *head;
+	pnode lista2 = NULL;
+	int size = 0;
+	while (current != NULL){
+		current = current->next;
+		size++;
+	}
+	//printf("%d \n", size);
+	current = *head;
+	if (size % 2 == 0){
+		for (int i = 1; i < size/2; i++){
+			current = current->next;
+		}
+		lista2 = current->next;
+		current->next = NULL;
+	} else{
+		for (int i = 0; i < size/2; i++){
+			current = current->next;
+		}
+		lista2 = current->next;
+		current->next = NULL;
+	}
+	return lista2;
+}
 
 int main(int argc, char const *argv[])
 {
 	pnode head = NULL;
-	for (int i = 0; i < 10; i++){
+	pnode lista2 = NULL;
+	for (int i = 0; i < 11; i++){
 		insertNode(&head, newNode(i));
 	}
 
-	insertPosX(&head, newNode(654), 1);
+	//insertPosX(&head, newNode(654), 3);
 	// removePosX(&head, 12);
 	//invertList(&head);
+	lista2 = splitList(&head);
 
 	printList(&head);
+	printList(&lista2);
 	return 0;
 }
