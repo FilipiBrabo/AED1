2be01ef2a1bd36746f1659bc339f4afde25595aa
FilipiBrabo <filipi.brabo@gmail.com>
2018-04-03 21:30:11 -0300

    Exercicio finalizado

--- lab-moodle/arvorenotas.c
+++ lab-moodle/arvorenotas.c
@@ -21,139 +21,132 @@ pTreeNode newTreeNode(int RA, int nota){
 	return x;
 }
 
-pTreeNode insert(pTreeNode root, int RA, int nota){
-	if (root == NULL)
-		return newTreeNode(RA, nota);
-
-	if (RA < root->RA){
-		root->left = insert(root->left, RA, nota);
-	}else if (RA > root->RA){
-		root->right = insert(root->right, RA, nota);
-	}
-	return root;
+void insertTreeNode(pTreeNode *root, pTreeNode x){
+	if (*root == NULL)
+		*root = x;
+	if (x->RA < (*root)->RA){ 			//Se o RA for menor vai pra esquerda
+		insertTreeNode(&(*root)->left, x);
+	}else if (x->RA > (*root)->RA){ 	//Se o RA for maior vai pra direita
+		insertTreeNode(&(*root)->right, x);
+	}else{ 								//RA igual
+		(*root)->nota = x->nota;
+	}	
 }
 
-int cont = 0;
+int numIteracoes = 0;
 
 pTreeNode searchRA(pTreeNode root, int RA){
 	if (root == NULL){
-		cont++;
-		printf("C=%d Nota=-1\n", cont);
-		cont = 0;
+		//numIteracoes++;
+		printf("C=%d Nota=-1\n", numIteracoes);
+		numIteracoes = 0;
 		return NULL; 
 	}
 	if (root->RA == RA){
-		cont++;
-		printf("C=%d Nota=%d", cont, root->nota);
-		cont = 0;
+		numIteracoes++;
+		printf("C=%d Nota=%d\n", numIteracoes, root->nota);
+		numIteracoes = 0;
 		return root;
 	}else if(RA < root->RA){
-		cont++;		
+		numIteracoes++;		
 		return searchRA(root->left, RA);
 	}else{
-		cont++;		
+		numIteracoes++;		
 		return searchRA(root->right, RA);
 	}
 
 }
-
-int height = -1;
-pTreeNode showHeight(pTreeNode root){
-	
-
+int max(int a, int b){
+	if (a > b){
+		return a;
+	}else{
+		return b;
+	}
 }
 
-//search the previous node to that RA
-pTreeNode previous;
-pTreeNode searchPrevious(pTreeNode root, int RA){	
+int findHeight(pTreeNode root){
 	if (root == NULL){
-		return NULL; 
+		return -1;
 	}
-	if (root->RA == RA){
-		return NULL;
-	}else if(RA < root->RA){
-		previous = root;
-		return searchPrevious(root->left, RA);
-	}else{
-		previous = root;
-		return searchPrevious(root->right, RA);
-	}
+	int leftHeight, rightHeight;
 
-
+	leftHeight = findHeight(root->left);
+	rightHeight = findHeight(root->right);
+	return max(leftHeight, rightHeight) + 1;
 }
 
-pTreeNode searchRightSmaller(pTreeNode x){
-	pTreeNode previous_aux;
-	if (x == NULL){
-		previous_aux = x;
-		return x;
-	}else{
-		previous_aux = x;
-		return searchRightSmaller(x->left);
-	}
-}
-
-pTreeNode removeTreeNode(pTreeNode root, int RA){
-	root = searchPrevious(root, RA);
-	if (root->right->RA == RA){
-		pTreeNode tmp = root->right;
-		
-		if (tmp->right == NULL && tmp->left == NULL){
-			root->right = NULL;
-			free(tmp);
-		}else if (tmp->right == NULL && tmp->left != NULL){
-			root->right = tmp->left;
-			free(tmp);
-		}else if(tmp->right != NULL && tmp->left == NULL){
-			root->right = tmp->right;
-			free(tmp);
-		}else{
-			pTreeNode smaller = searchRightSmaller(tmp);
-			tmp->RA = smaller->RA;
-			removeTreeNode(root);
-		}
-	}else if (root->left->RA == RA){
-		pTreeNode tmp = root->left;
-		
-		if (tmp->right == NULL && tmp->left == NULL){
-			root->left = NULL;
-			free(tmp);
-		}else if (tmp->right == NULL && tmp->left != NULL){
-			root->left = tmp->left;
-			free(tmp);
-		}else if(tmp->right != NULL && tmp->left == NULL){
-			root->left = tmp->right;
-			free(tmp);
-		}else{
-			pTreeNode smaller = searchRightSmaller(tmp);
-			tmp->RA = smaller->RA;
-			removeTreeNode(tmp->left, RA);
+void removeTreeNode(pTreeNode *root, int RA){
+	pTreeNode tmp = *root;
+	
+	//acha o anterior do nó a ser removido
+	pTreeNode prev = NULL;
+	while (tmp && tmp->RA != RA){
+		if (RA > tmp->RA){ //RA maior, vai para direita
+			prev = tmp;
+			tmp = tmp->right;
+		}else{  			//RA menor, vai para esquerda
+			prev = tmp;
+			tmp = tmp->left;
 		}
 	}
-}
+	
+	if (tmp){							//se existe o nó a ser removido				
+		if (tmp->right && tmp->left){ 	//se existirem 2 filhos
+			pTreeNode aux = NULL;
+			//acha o menor a direita
+			aux = tmp->right;
+			while (aux->left){
+				//prev = root;
+				aux = aux->left;
+			}
+			
+			tmp->RA = aux->RA;
+			tmp->nota = aux->nota;
+			
+			removeTreeNode(&(tmp->right), aux->RA);
 
+		}else{
+			pTreeNode aux2 = NULL;			        //existe no max um filho
+			if (tmp->right){				//existe o filho da direita
+				aux2 = tmp->right;
+			}else if (tmp->left){			//existe o filho da esquerda
+				aux2 = tmp->left;
+			}
+		
+			if(prev){ //se existe anterior(não estamos na raiz)
+				
+				//verificar se o nó está a esqueda ou a direita do anterior
+				if (tmp->RA > prev->RA){
+					prev->right = aux2;
+				}else{
+					prev->left = aux2;
+				}
+			}else{ //nó a ser removido é  a raiz
+				*root = aux2;
+			}
+			free(tmp);
+		}
+	}
+
+	
+}
 
 int main(int argc, char const *argv[])
 {
 	char op; //operation
-	int flag = 1, RA, nota;
+	int RA, nota, height;
 	pTreeNode root = NULL;
 
 	while(scanf("\n%c", &op) && op != 'P'){
 		switch(op){
 			case('I'):
-				scanf("%d %d", &RA, &nota);
-				if (flag == 1){
-					root = insert(root, RA, nota);
-					flag = 0;
-				}else{
-					insert(root, RA, nota);
-				}
+				scanf("%d %d", &RA, &nota);			
+				insertTreeNode(&root, newTreeNode(RA, nota));
 				break;
 
 			case('R'):
 				scanf("%d", &RA);
-				removeTreeNode(root, RA);
+				removeTreeNode(&root, RA);
 				break;
 
 			case('B'):
@@ -163,9 +156,11 @@ int main(int argc, char const *argv[])
 				break;
 
 			case('A'):
-				showHeight(root);
+				height = findHeight(root);
+				printf("A=%d\n", height);
 				break;
 		}
 	}
+
 	return 0;
 }
